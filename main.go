65963d7a6c51b078fdb553cc9a130338250fd782
mohosin16103308 <hamza1610330816@gmail.com>
2022-05-28 17:37:19 +0600

    Array V2

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 )
 func main() {
 	var n [10] int /* n is an array of 10 integers */
-	var i,j int
+	var i,j,k int
+
+	var friends = [] string{"MOHOSIN","SHARGO"}
  
 	/* initialize elements of array n to 0 */         
 	for i = 0; i < 10; i++ {
@@ -15,5 +17,11 @@ func main() {
 	/* output each array element's value */
 	for j = 0; j < 10; j++ {
 	   fmt.Printf("Element[%d] = %d\n", j, n[j] )
+	}
+
+	/* Out Put Array */
+
+	for k = 0; k < len(friends) ; k++ {
+		fmt.Println(friends[k])
 	}
  }
