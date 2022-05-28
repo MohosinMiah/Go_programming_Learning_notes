052282cdb4a03aaeb6f9d3e869e9a8bf9e8d4869
mohosin16103308 <hamza1610330816@gmail.com>
2022-05-28 17:41:12 +0600

    Array Two Diemntional array

--- main.go
+++ main.go
@@ -4,24 +4,18 @@ import (
 	"fmt"
 )
 func main() {
-	var n [10] int /* n is an array of 10 integers */
-	var i,j,k int
 
-	var friends = [] string{"MOHOSIN","SHARGO"}
- 
-	/* initialize elements of array n to 0 */         
-	for i = 0; i < 10; i++ {
-	   n[i] = i + 100 /* set element at location i to i + 100 */
-	}
+
+		/* an array with 5 rows and 2 columns*/
+		var a = [5][2]int{ {0,0}, {1,2}, {2,4}, {3,6},{4,8}}
+		var i, j int
+	 
+		/* output each array element's value */
+		for  i = 0; i < 5; i++ {
+		   for j = 0; j < 2; j++ {
+			  fmt.Printf("a[%d][%d] = %d\n", i,j, a[i][j] )
+		   }
 	
-	/* output each array element's value */
-	for j = 0; j < 10; j++ {
-	   fmt.Printf("Element[%d] = %d\n", j, n[j] )
-	}
-
-	/* Out Put Array */
-
-	for k = 0; k < len(friends) ; k++ {
-		fmt.Println(friends[k])
-	}
  }
+
+}
